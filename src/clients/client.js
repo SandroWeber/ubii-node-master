2b8d054938f1f4ff2de7be266838799ad78b949f
Sandro Weber <webers@in.tum.de>
2019-02-19 16:36:51 +0100

    working with web browser client

--- src/clients/client.js
+++ src/clients/client.js
@@ -155,20 +155,29 @@ class Client {
     // subscribe
     let token = this.topicData.subscribe(topic, (topic, data) => {
       console.info('### subscribe callback for client ' + this.name + ' - topic ' + topic);
-      console.info(data);
       let payload = {
+        deviceId: 'unused',
         topicDataRecord: {
           topic: topic
         }
       };
       //TODO: data.constructor.name.toLowerCase() is a hack, needs topic data format info implemented in ubii-topic-data
       let type = data.constructor.name.toLowerCase();
-      console.info(type);
       payload.topicDataRecord[type] = data;
+      console.info(payload);
 
-      let buffer = this.topicDataTranslator.createBufferFromPayload(payload);
+      try {
+        let buffer = this.topicDataTranslator.createBufferFromPayload(payload);
+        console.info(buffer);
+        console.info(buffer.length);
+        let messageTest = this.topicDataTranslator.createMessageFromBuffer(buffer);
+        console.info(messageTest);
 
-      this.sendMessageToRemote(buffer);
+        this.sendMessageToRemote(buffer);
+      } catch (error) {
+        console.error(error);
+      }
+
     });
 
     // save token
