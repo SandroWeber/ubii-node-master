20a13558a271e1897e66d93ecd8d0fa8901aaf76
Daniel <dyrda@in.tum.de>
2019-05-17 12:20:49 +0200

    Implemented sessionReplaceService

---
+++ src/services/sessions/sessionReplaceService.js
@@ -0,0 +1,43 @@
+const {Service} = require('./../service.js');
+const SessionDatabase = require('../../storage/sessionDatabase');
+
+const { DEFAULT_TOPICS } = require('@tum-far/ubii-msg-formats');
+
+class SessionReplaceService extends Service {
+  constructor() {
+    super(DEFAULT_TOPICS.SERVICES.SESSION_REPLACE);
+  }
+
+  reply(sessionMessage) {
+    if (!SessionDatabase.verifySpecification(sessionMessage)) {
+      return {
+        error: {
+          title: 'SessionGetService Error',
+          message: 'Could not verify session with ID ' + sessionMessage.id
+        }
+      };
+    }
+
+    try {
+      SessionDatabase.updateSessionSpecs(sessionMessage);
+    } catch (error) {
+      return {
+        error: {
+          title: 'SessionGetService Error',
+          message: error.toString()
+        }
+      };
+    }
+
+    return {
+      success: {
+        title: 'SessionReplaceService',
+        message: 'Successfully replaced session with ID ' + sessionMessage.id
+      }
+    };
+  }
+}
+
+module.exports = {
+  'SessionReplaceService': SessionReplaceService
+};
