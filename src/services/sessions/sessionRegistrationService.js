98a8d2109c7d5d9f644d4661cf7b8481a3bb223e
Daniel <dyrda@in.tum.de>
2019-05-20 14:09:45 +0200

    Reworked sessionRegistrationService

--- src/services/sessions/sessionRegistrationService.js
+++ src/services/sessions/sessionRegistrationService.js
@@ -4,31 +4,53 @@ const SessionDatabase = require('../../storage/sessionDatabase');
 const { DEFAULT_TOPICS } = require('@tum-far/ubii-msg-formats');
 
 class SessionRegistrationService extends Service {
-  constructor(sessionManager) {
-    super(DEFAULT_TOPICS.SERVICES.SESSION_REGISTRATION);
-
-    this.sessionManager = sessionManager;
+  constructor() {
+    super(DEFAULT_TOPICS.SERVICES.INTERACTION_REGISTRATION);
   }
 
-  reply(message) {
+  reply(sessionSpecs) {
+    if (typeof sessionSpecs === 'undefined') {
+      return {
+        error: {
+          title: 'SessionRegistrationService Error',
+          message: 'Session specifications are undefined.'
+        }
+      };
+    }
+
+    if (Array.isArray(sessionSpecs)) {
+      let newSessions = [];
+      sessionSpecs.forEach((spec) => {
+        try {
+          newSessions.push(SessionDatabase.addSession(spec));
+        } catch (error) {
+          return {
+            error: {
+              title: 'SessionRegistrationService Error',
+              message: error.toString()
+            }
+          };
+        }
+      });
+
+      return {
+        sessionList: newSessions.map((session) => {return session.toProtobuf()})
+      };
+    }
+
+    let session;
     try {
-      let session = this.sessionManager.createSession(message);
-      SessionDatabase.saveSessionSpecsToFile(session.toProtobuf());
+      session = SessionDatabase.addSession(sessionSpecs);
     } catch (error) {
       return {
         error: {
           title: 'SessionRegistrationService Error',
           message: error.toString()
         }
-      }
+      };
     }
 
-    return {
-      success: {
-        title: 'SessionRegistrationService Success',
-        message: 'Saved session with ID ' + message.id + ' to database'
-      }
-    }
+    return {session: session.toProtobuf()};
   }
 }
 
