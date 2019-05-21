58ae17205a611ede7ebd1914cd83dbaa931f137a
Daniel <dyrda@in.tum.de>
2019-05-21 11:06:39 +0200

    Fixed wrong service topic bug

--- src/services/sessions/sessionRegistrationService.js
+++ src/services/sessions/sessionRegistrationService.js
@@ -5,7 +5,7 @@ const { DEFAULT_TOPICS } = require('@tum-far/ubii-msg-formats');
 
 class SessionRegistrationService extends Service {
   constructor(sessionManager) {
-    super(DEFAULT_TOPICS.SERVICES.INTERACTION_REGISTRATION);
+    super(DEFAULT_TOPICS.SERVICES.SESSION_REGISTRATION);
 
     this.sessionManager = sessionManager;
   }
