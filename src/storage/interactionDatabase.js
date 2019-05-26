c3abb6e1cfb8af2e2fe4564861d88e12e2c2d082
daniel <dyrda@in.tum.de>
2019-05-26 14:04:16 +0200

    Updated updateInteraction method name

--- src/storage/interactionDatabase.js
+++ src/storage/interactionDatabase.js
@@ -65,7 +65,7 @@ class InteractionDatabase extends Storage{
    * Update a interaction specification that is already present in the specifications list with a new value.
    * @param {Object} specification The specification requires a name and id property.
    */
-  updateInteractionSpecs(specification) {
+  updateInteraction(specification) {
     if (!this.verifySpecification(specification)) {
       throw 'interaction specification could not be verified';
     }
