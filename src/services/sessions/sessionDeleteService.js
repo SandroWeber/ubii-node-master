9ebbf0cbe256c7092bc941ed1732ddd1d43419aa
daniel <dyrda@in.tum.de>
2019-05-26 14:07:52 +0200

    Fixed error message object bug

--- src/services/sessions/sessionDeleteService.js
+++ src/services/sessions/sessionDeleteService.js
@@ -24,7 +24,7 @@ class SessionDeleteService extends Service {
         return {
           error: {
             title: 'SessionDeleteService Error',
-            message: error
+            message: error.toString()
           }
         };
       }
