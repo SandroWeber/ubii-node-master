f509f99adf2fc22e6303c2c5cbe8033947da3461
daniel <dyrda@in.tum.de>
2019-05-29 15:48:29 +0200

    Added interactionDatabaseGetService

---
+++ src/services/interactions/interactionDatabaseGetService.js
@@ -0,0 +1,28 @@
+const {Service} = require('./../service.js');
+const InteractionDatabase = require('../../storage/interactionDatabase');
+
+const { DEFAULT_TOPICS } = require('@tum-far/ubii-msg-formats');
+
+class InteractionDatabaseGetService extends Service {
+  constructor() {
+    super(DEFAULT_TOPICS.SERVICES.INTERACTION_DATABASE_GET);
+  }
+
+  reply(interactionMessage) {
+    let interaction = InteractionDatabase.getInteraction(interactionMessage.id);
+    if (typeof interaction === 'undefined') {
+      return {
+        error: {
+          title: 'InteractionDatabaseGetService Error',
+          message: 'Could not find interaction with ID ' + interactionMessage.id
+        }
+      };
+    } else {
+      return {interaction: interaction.toProtobuf()};
+    }
+  }
+}
+
+module.exports = {
+  'InteractionDatabaseGetService': InteractionDatabaseGetService
+};
