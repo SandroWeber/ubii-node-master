b0423a3323e53032333beabfebb1680d3c96a74b
daniel <dyrda@in.tum.de>
2019-05-29 15:49:53 +0200

    Added interactionRuntimeGetlistService

---
+++ src/services/interactions/interactionRuntimeGetListService.js
@@ -0,0 +1,30 @@
+const {Service} = require('./../service.js');
+const InteractionDatabase = require('../../storage/interactionDatabase');
+
+const { DEFAULT_TOPICS } = require('@tum-far/ubii-msg-formats');
+
+class InteractionRuntimeGetListService extends Service {
+  constructor() {
+    super(DEFAULT_TOPICS.SERVICES.INTERACTION_RUNTIME_GET_LIST);
+  }
+
+  reply() {
+    let interactions = InteractionDatabase.getInteractionList();
+    if (typeof interactions === 'undefined') {
+      return {
+        error: {
+          title: 'InteractionRuntimeGetListService Error',
+          message: 'Could not retrieve interaction list'
+        }
+      };
+    } else {
+      return {
+        interactionList: interactions
+      };
+    }
+  }
+}
+
+module.exports = {
+  'InteractionRuntimeGetListService': InteractionRuntimeGetListService
+};
