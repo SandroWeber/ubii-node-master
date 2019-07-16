3812eabf4c0ae37f132cbea7a784e2799fd02264
Sandro Weber <webers@in.tum.de>
2019-07-16 16:19:53 +0200

    device deregistration service

---
+++ src/services/devices/deviceDeregistrationService.js
@@ -0,0 +1,63 @@
+const {
+  Service
+} = require('../service.js');
+const namida = require("@tum-far/namida");
+
+const { DEFAULT_TOPICS } = require('@tum-far/ubii-msg-formats');
+
+class DeviceDeregistrationService extends Service {
+  constructor(clientManager, deviceManager) {
+    super(DEFAULT_TOPICS.SERVICES.DEVICE_DEREGISTRATION);
+
+    this.clientManager = clientManager;
+    this.deviceManager = deviceManager;
+  }
+
+  reply(message) {
+    let context = this.prepareContext();
+    context.success = false;
+
+    // Verify the device and act accordingly.
+    if (!this.clientManager.verifyClient(message.clientId)) {
+      // Prepare the context.
+      // Update the context feedback.
+      context.feedback.message = `There is no Client registered with the id ${namida.style.messageHighlight(message.clientId)}. ` +
+        `Message processing was aborted due to an unregistered client.`;
+      context.feedback.title = 'DeviceDeregistrationService ERROR';
+
+      namida.logFailure(context.feedback.title, context.feedback.message);
+
+      return {
+        error: {
+          title: context.feedback.title,
+          message: context.feedback.message,
+          stack: context.feedback.stack
+        }
+      };
+    }
+
+    // Process the registration of the sepcified device at the device manager
+    try {
+      device = this.deviceManager.removeDevice(message.id);
+
+      return {
+        success: {
+          title: 'DeviceDeregistrationService SUCCESS',
+          message: 'Device with ID ' + message.id + ' successfully removed.'
+        }
+      }
+    } catch (error) {
+      return {
+        error: {
+          title: 'DeviceDeregistrationService ERROR',
+          message: error && error.toString(),
+          stack: error.stack && error.stack.toString()
+        }
+      };
+    }
+  }
+}
+
+module.exports = {
+  'DeviceDeregistrationService': DeviceDeregistrationService,
+};
