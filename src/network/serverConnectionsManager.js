73b5f0f2d73f5d3f5335badb1fca35f74c66735f
Sandro Weber <webers@in.tum.de>
2019-07-23 12:35:42 +0100

    hotfix ipconfig selection

--- src/network/serverConnectionsManager.js
+++ src/network/serverConnectionsManager.js
@@ -46,7 +46,7 @@ class ServerConnectionsManager {
     let ifaces = os.networkInterfaces();
     Object.keys(ifaces).forEach((ifname) => {
       ifaces[ifname].forEach((iface) => {
-        if (iface.family === 'IPv4' && !iface.internal) {
+        if (iface.family === 'IPv4' && !iface.internal && ifname.indexOf('Default Switch') === -1) {
           if (ifname.indexOf('en') === 0 || ifname.indexOf('vEthernet') === 0 || ifname.indexOf('Ethernet') === 0) {
             this.hostAdresses.ethernet = iface.address;
           } else if (ifname.indexOf('wl') === 0 || ifname.indexOf('Wi-Fi') === 0) {
