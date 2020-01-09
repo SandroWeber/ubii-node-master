616f63b06ed5791a58c19b752e89a8a3e1f2efb8
Sandro Weber <webers@in.tum.de>
2020-01-10 00:41:35 +0100

    WIP

--- src/sessions/sessionManager.js
+++ src/sessions/sessionManager.js
@@ -14,9 +14,7 @@ class SessionManager extends EventEmitter {
     this.deviceManager = deviceManager;
     this.sessions = [];
 
-    this.on(EVENTS_SESSION_MANAGER.NEW_SESSION, specs => {
-      this.onEventNewSession(specs);
-    });
+    this.addEventListeners();
   }
 
   createSession(specifications = {}) {
@@ -48,7 +46,9 @@ class SessionManager extends EventEmitter {
   removeSession(id) {
     for (let i = 0; i < this.sessions.length; i++) {
       if (this.sessions[i].id === id) {
+        let sessionSpecs = this.sessions[i].toProtobuf();
         this.sessions.splice(i, 1);
+        this.emit(EVENTS_SESSION_MANAGER.DELETE_SESSION, sessionSpecs);
         break;
       }
     }
@@ -108,9 +108,41 @@ class SessionManager extends EventEmitter {
     });
   }
 
+  /* event related functions */
+
+  addEventListeners() {
+    this.on(EVENTS_SESSION_MANAGER.NEW_SESSION, specs => {
+      this.onEventNewSession(specs);
+    });
+
+    this.on(EVENTS_SESSION_MANAGER.CHANGE_SESSION, specs => {
+      this.onEventSessionChange(specs);
+    });
+
+    this.on(EVENTS_SESSION_MANAGER.DELETE_SESSION, specs => {
+      this.onEventSessionChange(specs);
+    });
+  }
+
   onEventNewSession(sessionSpecs) {
     this.topicData.publish(
       DEFAULT_TOPICS.INFO_TOPICS.NEW_SESSION,
+      sessionSpecs,
+      Utils.getTopicDataTypeFromMessageFormat(MSG_TYPES.SESSION)
+    );
+  }
+
+  onEventSessionChange(sessionSpecs) {
+    this.topicData.publish(
+      DEFAULT_TOPICS.INFO_TOPICS.CHANGE_SESSION,
+      sessionSpecs,
+      Utils.getTopicDataTypeFromMessageFormat(MSG_TYPES.SESSION)
+    );
+  }
+
+  onEventSessionDelete(sessionSpecs) {
+    this.topicData.publish(
+      DEFAULT_TOPICS.INFO_TOPICS.DELETE_SESSION,
       sessionSpecs,
       Utils.getTopicDataTypeFromMessageFormat(MSG_TYPES.SESSION)
     );
