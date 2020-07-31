b18a33edcddb8e3097e3f05551ea755a36d58527
Sandro Weber <webers@in.tum.de>
2020-07-31 18:00:40 +0200

    WIP

--- src/clients/client.js
+++ src/clients/client.js
@@ -9,9 +9,9 @@ const uuidv4 = require('uuid/v4');
 const { TOPIC_EVENTS } = require('@tum-far/ubii-topic-data');
 
 let clientStateEnum = Object.freeze({
-  "active": "active",
-  "standby": "standby",
-  "inactive": "inactive"
+  active: 'active',
+  standby: 'standby',
+  inactive: 'inactive'
 });
 
 const { ProtobufTranslator, MSG_TYPES } = require('@tum-far/ubii-msg-formats');
@@ -31,7 +31,9 @@ class Client {
     this.registrationDate = new Date();
     this.lastSignOfLife = null;
     this.topicSubscriptionTokens = new Map();
-    this.regexSubscriptionTokens = new Map();
+    this.explicitTopicSubscriptions = [];
+    this.topicSubscriptions = new Map();
+    this.regexSubscriptions = new Map();
 
     this.topicDataTranslator = new ProtobufTranslator(MSG_TYPES.TOPIC_DATA);
   }
@@ -105,45 +107,53 @@ class Client {
       try {
         this.updateLastSignOfLife();
       } catch (e) {
-        namida.error('UpdateLastSignOfLife failed',
+        namida.error(
+          'UpdateLastSignOfLife failed',
           `UpdateLastSignOfLife of client with id ${this.id} failed with an error.`,
-          '' + (e.stack || e));
+          '' + (e.stack || e)
+        );
       }
-    }
+    };
 
     // Ping the remote for the first time
     this.pingRemote(() => {
       signOfLifePingCallback();
-    })
+    });
 
     // Start an interval for regular pings and state checks.
     this.signOfLifeInterval = setInterval(() => {
       // Determine the time since the last sign of life.
       let now = new Date();
-      let difference = (now - this.lastSignOfLife);
+      let difference = now - this.lastSignOfLife;
 
       // Determine the current state. If the state changes, ouput the feedback on the server console.
       if (difference > TIME_UNTIL_STANDBY) {
         if (difference > TIME_UNTIL_INACTIVE) {
           // The client has the state inactive.
           if (this.state !== clientStateEnum.inactive) {
-            namida.log(`Client State has changed`,
-              `Client with id ${this.id} is not available and is now in an inactive state.`);
+            namida.log(
+              `Client State has changed`,
+              `Client with id ${this.id} is not available and is now in an inactive state.`
+            );
           }
           this.state = clientStateEnum.inactive;
         } else {
           // The client has the state standby.
           if (this.state !== clientStateEnum.standby) {
-            namida.log(`Client State has changed`,
-              `Client with id ${this.id} is not available and is now in an standby state.`);
+            namida.log(
+              `Client State has changed`,
+              `Client with id ${this.id} is not available and is now in an standby state.`
+            );
           }
           this.state = clientStateEnum.standby;
         }
       } else {
         // The client has the state active.
         if (this.state !== clientStateEnum.active) {
-          namida.log(`Client State has changed`,
-            `Client with id ${this.id} is available again and is now in an active state.`);
+          namida.log(
+            `Client State has changed`,
+            `Client with id ${this.id} is available again and is now in an active state.`
+          );
         }
         this.state = clientStateEnum.active;
       }
@@ -164,14 +174,14 @@ class Client {
   }
 
   /**
-   * Subscribe to a topic at the topicData
+   * Internally subscribe to a topic at the topicData
    * @param {String} topic
    */
-  subscribe(topic) {
+  subscribeAtTopicData(topic) {
     if (this.topicSubscriptionTokens.has(topic)) {
-      namida.logFailure(`Topic Data subscription rejected`,
-        `Client with id ${this.id} is already subscribed to this topic.`);
-      return;
+      /*namida.logFailure(`Topic Data subscription rejected`,
+        `Client with id ${this.id} is already subscribed to this topic.`);*/
+      return false;
     }
 
     // subscribe
@@ -193,17 +203,21 @@ class Client {
 
     // save token
     this.topicSubscriptionTokens.set(topic, token);
+
+    return true;
   }
 
   /**
-   * Unsubscribes from a topic at the topicData.
+   * Internally unsubscribes from a topic at the topicData.
    * @param {String} topic
    */
-  unsubscribe(topic) {
+  unsubscribeAtTopicData(topic) {
     if (!this.topicSubscriptionTokens.has(topic)) {
-      namida.logFailure(`Topic Data unsubscription rejected`,
-        `Client with ID ${this.id} is not subscribed to this topic.`);
-      return;
+      /*namida.logFailure(
+        `Topic Data unsubscription rejected`,
+        `Client with ID ${this.id} is not subscribed to this topic.`
+      );*/
+      return false;
     }
 
     // get token
@@ -213,82 +227,184 @@ class Client {
     this.topicData.unsubscribe(token);
     // remove token
     this.topicSubscriptionTokens.delete(topic);
-  }
-
-  /**
-   * Subscribe to a regex at topicData
-   * @param {String} regexString
-   */
-  subscribeRegex(regexString) {
-    if (this.regexSubscriptionTokens.has(regexString)) {
-      namida.logFailure(`RegExp subscription rejected`,
-        `Client with id ${this.id} is already subscribed to this RegExp.`);
-      return false;
-    }
-
-    // subscribe
-    let regex = new RegExp(regexString);
-
-    let subscriptionCallback = (topic) => {
-      if (regex.test(topic)) {
-        this.subscribe(topic);
-      }
-    };
-
-    // auto-subscribe on new matching topics
-    this.topicData.events.addListener(TOPIC_EVENTS.NEW_TOPIC, subscriptionCallback);
-
-    this.topicData.getAllTopicsWithData().map(entry => entry.topic).forEach(subscriptionCallback);
-
-    let token = {
-      'regexString': regexString,
-      'regex': regex,
-      'id': this.id,
-      'type': 'single',
-      'subscriptionCallback': subscriptionCallback
-    };
-
-    // save token
-    this.regexSubscriptionTokens.set(regexString, token);
 
     return true;
   }
 
   /**
-   * Unsubscribe from a regex at topicData
-   * @param {String} regexString
+   * Subscribe to a topic, to be called from outside when subscribing to a single topic
+   * @param {String} topic
    */
-  unsubscribeRegex(regexString) {
-    if (!this.regexSubscriptionTokens.has(regexString)) {
-      namida.logFailure(`RegExp unsubscription rejected`,
-        `Client with ID ${this.id} is not subscribed to this RegExp.`);
+  subscribeTopic(topic) {
+    let subscription = this.topicSubscriptions.get(topic);
+    if (subscription && subscription.explicit) {
+      namida.logFailure(
+        `Topic Data subscription rejected`,
+        `Client with ID ${this.id} is already subscribed to topic ${topic}.`
+      );
       return;
     }
 
-    // get token
-    let regexToken = this.regexSubscriptionTokens.get(regexString);
+    if (!subscription) {
+      let success = this.subscribeAtTopicData(topic);
+      // successfully subscribed just now or already subscribed by some other means
+      if (success) {
+        // add explicit subscription to topic
+        this.topicSubscriptions.set(topic, {
+          explicit: true,
+          regExes: []
+        });
+      }
+    } else {
+      subscription.explicit = true;
+    }
+  }
 
-    // remove subscriptionCallback
-    this.topicData.events.removeListener(TOPIC_EVENTS.NEW_TOPIC, regexToken.subscriptionCallback);
+  /**
+   * Unsubscribe from a topic, to be called from outside when unsubscribing from a single topic
+   * @param {String} topic
+   */
+  unsubscribeTopic(topic) {
+    let subscription = this.topicSubscriptions.get(topic);
 
-    // unsubscribe
-    let topicList = [];
-    this.topicSubscriptionTokens.forEach((token) => {
-      if (regexToken.regex.test(token.topic)) {
-        topicList.push(token.topic);
+    // no (explicit) subscription?
+    if (!subscription || !subscription.explicit) {
+      namida.logFailure(
+        `Topic Data unsubscription rejected`,
+        `Client with ID ${this.id} is not subscribed to topic ${topic}.`
+      );
+      return;
+    }
+
+    // change subscription not to be explicit
+    subscription.explicit = false;
+    // check if no subscription remaining, if so completely unsub at TopicData and delete entry
+    if (subscription.regExes.length === 0) {
+      this.unsubscribeAtTopicData(topic);
+      this.topicSubscriptions.delete(topic);
+    }
+  }
+
+  /*hasExplicitTopicSubscription(topic) {
+    return this.explicitTopicSubscriptions.includes(topic);
+  }*/
+
+  /**
+   * Subscribe to a regular expression
+   * @param {String} regexString
+   */
+  subscribeRegex(regexString) {
+    if (this.regexSubscriptions.has(regexString)) {
+      namida.logFailure(
+        `RegExp subscription rejected`,
+        `Client with id ${this.id} is already subscribed to this RegExp.`
+      );
+      return false;
+    }
+
+    let regex = new RegExp(regexString);
+
+    // callback to check if necessary to subscribe to certain topic
+    let checkSubscriptionForTopic = (topic) => {
+      if (regex.test(topic)) {
+        let subscription = this.topicSubscriptions.get(topic);
+
+        // no subscription to this topic yet?
+        if (!subscription) {
+          let success = this.subscribeAtTopicData(topic);
+          if (success) {
+            // add new subscription to map, regex only for now
+            this.topicSubscriptions.set(topic, {
+              explicit: false,
+              regExes: [regexString]
+            });
+          }
+        }
+        // already subscribed explicitly, add subscription via regex
+        else {
+          subscription.regExes.push(regexString);
+        }
+      }
+    };
+
+    // save regexSubscriptionData
+    let regexSubscriptionData = {
+      regexString: regexString,
+      regex: regex,
+      id: this.id,
+      type: 'single',
+      checkSubscriptionForTopic: checkSubscriptionForTopic
+    };
+    this.regexSubscriptions.set(regexString, regexSubscriptionData);
+
+    // auto-subscribe on new matching topics
+    this.topicData.events.addListener(TOPIC_EVENTS.NEW_TOPIC, checkSubscriptionForTopic);
+
+    // add subscriptions to existing topics
+    this.topicData
+      .getAllTopicsWithData()
+      .map((entry) => entry.topic)
+      .forEach(checkSubscriptionForTopic);
+
+    return true;
+  }
+
+  /**
+   * Unsubscribe from a regular expression
+   * @param {String} regexString
+   */
+  unsubscribeRegex(regexString) {
+    if (!this.regexSubscriptions.has(regexString)) {
+      namida.logFailure(
+        `RegExp unsubscription rejected`,
+        `Client with ID ${this.id} is not subscribed to RegExp ${regexString}.`
+      );
+      return;
+    }
+
+    // get subscription data
+    let regexSubscriptionData = this.regexSubscriptions.get(regexString);
+
+    // remove checkSubscriptionForTopic
+    this.topicData.events.removeListener(
+      TOPIC_EVENTS.NEW_TOPIC,
+      regexSubscriptionData.checkSubscriptionForTopic
+    );
+
+    // remove regex subscription
+    this.regexSubscriptions.delete(regexString);
+
+    // check topic subscriptions
+    this.topicSubscriptions.forEach((subs, topic) => {
+      let index = subs.regExes.indexOf(regexString);
+      if (index !== -1) {
+        subs.regExes.splice(index, 1);
+      }
+
+      // if no subscriptions remain, unsubscribe at TopicData
+      if (!subs.explicit && subs.regExes.length === 0) {
+        this.unsubscribeAtTopicData(topic);
       }
     });
-    topicList.forEach(topic => {
-      this.unsubscribe(topic);
-    });
-    // remove token
-    this.regexSubscriptionTokens.delete(regexString);
+  }
+
+  hasRegexSubscription(topic) {
+    let sub = this.topicSubscriptions.get(topic);
+
+    if (sub) {
+      return sub.regExes.length > 0;
+    }
+
+    return false;
   }
 
   unsubscribeAll() {
     for (let token in this.topicSubscriptionTokens) {
       this.topicData.unsubscribe(token);
     }
+    this.topicSubscriptionTokens.clear();
+    this.topicSubscriptions.clear();
+    this.regexSubscriptions.clear();
   }
 
   toProtobuf() {
@@ -298,11 +414,11 @@ class Client {
       devices: this.devices,
       tags: this.tags,
       description: this.description
-    }
+    };
   }
 }
 
 module.exports = {
-  'Client': Client,
-  'clientStateEnum': clientStateEnum,
+  Client: Client,
+  clientStateEnum: clientStateEnum
 };
--- test/clients/integration-tests/regexSubscriptions.js
+++ test/clients/integration-tests/regexSubscriptions.js
@@ -5,117 +5,154 @@ import { Client } from '../../../src/index.js';
 import { ServerMock } from '../../mocks/serverMock';
 
 (function () {
-    // Helpers:
+  // Helpers:
 
-    // Preparation:
+  // Preparation:
 
-    test.beforeEach(t => {
-        t.context.topicData = new RuntimeTopicData();
-        t.context.server = new ServerMock();
-        t.context.client = new Client({}, t.context.server, t.context.topicData);
+  test.beforeEach((t) => {
+    t.context.topicData = new RuntimeTopicData();
+    t.context.server = new ServerMock();
+    t.context.client = new Client({}, t.context.server, t.context.topicData);
 
-        t.context.topics = [
-            // 0
-            {
-                topic: '/my/topics/a'
-            },
-            // 1
-            {
-                topic: '/my/topics/b'
-            },
-            // 2
-            {
-                topic: '/my/topics/c'
-            },
-            // 3
-            {
-                topic: '/other/topics/a'
-            },
-            // 4
-            {
-                topic: '/other/topics/b'
-            }
-        ]
-    });
+    t.context.topics = [
+      // 0
+      {
+        topic: '/my/topics/a'
+      },
+      // 1
+      {
+        topic: '/my/topics/b'
+      },
+      // 2
+      {
+        topic: '/my/topics/c'
+      },
+      // 3
+      {
+        topic: '/other/topics/a'
+      },
+      // 4
+      {
+        topic: '/other/topics/b'
+      }
+    ];
+  });
 
-    // Test cases:
+  // Test cases:
 
-    test('subscribeRegex', t => {
-        let client = t.context.client;
-        let topicData = t.context.topicData;
-        let topics = t.context.topics;
+  test('subscribeRegex', (t) => {
+    let client = t.context.client;
+    let topicData = t.context.topicData;
+    let topics = t.context.topics;
 
-        let regexString = '/my/topics/*';
+    let regexString = '/my/topics/*';
 
-        // pre-publish some topics
-        topicData.publish(topics[0].topic, true, 'boolean');
-        topicData.publish(topics[1].topic, true, 'boolean');
-        topicData.publish(topics[3].topic, true, 'boolean');
+    // pre-publish some topics
+    topicData.publish(topics[0].topic, true, 'boolean');
+    topicData.publish(topics[1].topic, true, 'boolean');
+    topicData.publish(topics[3].topic, true, 'boolean');
 
-        // subscribe regex
-        client.subscribeRegex(regexString);
-        t.true(client.regexSubscriptionTokens.has(regexString));
+    // subscribe regex
+    client.subscribeRegex(regexString);
+    t.true(client.regexSubscriptions.has(regexString));
 
-        // check subscriptions to existing topics matching regex
-        t.true(client.topicSubscriptionTokens.has(topics[0].topic));
-        t.true(client.topicSubscriptionTokens.has(topics[1].topic));
-        t.false(client.topicSubscriptionTokens.has(topics[3].topic));
+    // check subscriptions to existing topics matching regex
+    t.true(client.topicSubscriptionTokens.has(topics[0].topic));
+    t.true(client.topicSubscriptionTokens.has(topics[1].topic));
+    t.false(client.topicSubscriptionTokens.has(topics[3].topic));
 
-        // publish other topics
-        topicData.publish(topics[2].topic, true, 'boolean');
-        topicData.publish(topics[4].topic, true, 'boolean');
+    // publish other topics
+    topicData.publish(topics[2].topic, true, 'boolean');
+    topicData.publish(topics[4].topic, true, 'boolean');
 
-        // check new subscriptions
-        t.true(client.topicSubscriptionTokens.has(topics[2].topic));
-        t.false(client.topicSubscriptionTokens.has(topics[4].topic));
-    });
+    // check new subscriptions
+    t.true(client.topicSubscriptionTokens.has(topics[2].topic));
+    t.false(client.topicSubscriptionTokens.has(topics[4].topic));
+  });
 
-    test('subscribeRegex - double subscribe', t => {
-        let client = t.context.client;
+  test('subscribeRegex - double subscribe', (t) => {
+    let client = t.context.client;
 
-        let regexString = '/my/topics/*';
+    let regexString = '/my/topics/*';
 
-        let success = client.subscribeRegex(regexString);
-        t.true(success);
+    let success = client.subscribeRegex(regexString);
+    t.true(success);
 
-        success = client.subscribeRegex(regexString);
-        t.false(success);
-    });
+    success = client.subscribeRegex(regexString);
+    t.false(success);
+  });
 
-    test('unsubscribeRegex', t => {
-        let client = t.context.client;
-        let topicData = t.context.topicData;
-        let topics = t.context.topics;
-        let server = t.context.server;
+  test('unsubscribeRegex', (t) => {
+    let client = t.context.client;
+    let topicData = t.context.topicData;
+    let topics = t.context.topics;
+    let server = t.context.server;
 
-        let regexString = '/my/topics/*';
+    let regexString = '/my/topics/*';
 
-        // subscribe
-        client.subscribeRegex(regexString);
-        //publish
-        topicData.publish(topics[0].topic, true, 'boolean');
-        topicData.publish(topics[1].topic, true, 'boolean');
-        // check subscriptions to existing topics matching regex
-        t.true(client.topicSubscriptionTokens.has(topics[0].topic));
-        t.true(client.topicSubscriptionTokens.has(topics[1].topic));
+    // subscribe
+    client.subscribeRegex(regexString);
+    //publish
+    topicData.publish(topics[0].topic, true, 'boolean');
+    topicData.publish(topics[1].topic, true, 'boolean');
+    // check subscriptions to existing topics matching regex
+    t.true(client.topicSubscriptionTokens.has(topics[0].topic));
+    t.true(client.topicSubscriptionTokens.has(topics[1].topic));
 
-        // unsubscribe
-        let callCountSendBeforeUnsubscribe = server.send.callCount;
-        client.unsubscribeRegex(regexString);
-        // check that subscriptions are gone
-        t.false(client.topicSubscriptionTokens.has(topics[0].topic));
-        t.false(client.topicSubscriptionTokens.has(topics[1].topic));
-        //publish again
-        topicData.publish(topics[0].topic, true, 'boolean');
-        topicData.publish(topics[1].topic, true, 'boolean');
-        // should not send out any more messages
-        t.is(server.send.callCount, callCountSendBeforeUnsubscribe);
-        // check subscriptions again, should not resubscribe after publishing
-        t.false(client.topicSubscriptionTokens.has(topics[0].topic));
-        t.false(client.topicSubscriptionTokens.has(topics[1].topic));
+    // unsubscribe
+    let callCountSendBeforeUnsubscribe = server.send.callCount;
+    client.unsubscribeRegex(regexString);
+    // check that subscriptions are gone
+    t.false(client.topicSubscriptionTokens.has(topics[0].topic));
+    t.false(client.topicSubscriptionTokens.has(topics[1].topic));
+    //publish again
+    topicData.publish(topics[0].topic, true, 'boolean');
+    topicData.publish(topics[1].topic, true, 'boolean');
+    // should not send out any more messages
+    t.is(server.send.callCount, callCountSendBeforeUnsubscribe);
+    // check subscriptions again, should not resubscribe after publishing
+    t.false(client.topicSubscriptionTokens.has(topics[0].topic));
+    t.false(client.topicSubscriptionTokens.has(topics[1].topic));
 
-        // publish new topic matching regex, should not subscribe to it
-        topicData.publish(topics[2].topic, true, 'boolean');
-        t.false(client.topicSubscriptionTokens.has(topics[2].topic));
-    });
+    // publish new topic matching regex, should not subscribe to it
+    topicData.publish(topics[2].topic, true, 'boolean');
+    t.false(client.topicSubscriptionTokens.has(topics[2].topic));
+  });
+
+  test('regex and regular topic subscriptions overlapping', (t) => {
+    let client = t.context.client;
+    let topicData = t.context.topicData;
+    let topics = t.context.topics;
+    let server = t.context.server;
+
+    let regexString = '/my/topics/*';
+
+    // subscribe
+    client.subscribeRegex(regexString);
+    //publish
+    topicData.publish(topics[0].topic, true, 'boolean');
+    topicData.publish(topics[1].topic, true, 'boolean');
+    // check subscriptions to existing topics matching regex
+    t.true(client.topicSubscriptionTokens.has(topics[0].topic));
+    t.true(client.topicSubscriptionTokens.has(topics[1].topic));
+
+    // unsubscribe
+    let callCountSendBeforeUnsubscribe = server.send.callCount;
+    client.unsubscribeRegex(regexString);
+    // check that subscriptions are gone
+    t.false(client.topicSubscriptionTokens.has(topics[0].topic));
+    t.false(client.topicSubscriptionTokens.has(topics[1].topic));
+    //publish again
+    topicData.publish(topics[0].topic, true, 'boolean');
+    topicData.publish(topics[1].topic, true, 'boolean');
+    // should not send out any more messages
+    t.is(server.send.callCount, callCountSendBeforeUnsubscribe);
+    // check subscriptions again, should not resubscribe after publishing
+    t.false(client.topicSubscriptionTokens.has(topics[0].topic));
+    t.false(client.topicSubscriptionTokens.has(topics[1].topic));
+
+    // publish new topic matching regex, should not subscribe to it
+    topicData.publish(topics[2].topic, true, 'boolean');
+    t.false(client.topicSubscriptionTokens.has(topics[2].topic));
+  });
 })();
