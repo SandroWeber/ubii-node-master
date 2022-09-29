97ee6574ef1c777ef317789e672da97de4a85b5f
Sandro Weber <webers@in.tum.de>
2022-09-29 18:34:10 +0200

    moved relative topicdata measurements to profiler

--- src/profiling/profiler.js
+++ src/profiling/profiler.js
@@ -8,11 +8,16 @@ class Profiler {
 
     this.profilingConfig = ConfigService.instance.config.profiling;
 
+    this.tStartup = Date.now();
+
     this.topicStatsMsgsPerSecondReceived = this.node.id + '/stats/msgs_per_second_received';
     this.topicStatsMsgsPerSecondSent = this.node.id + '/stats/msgs_per_second_sent';
 
     this.msIntervalProduceStats = 5000;
     this.intervalProduceStats = setInterval(() => this.produceStatistics(), this.msIntervalProduceStats);
+
+    this.topicdataReceivedPrevious = 0;
+    this.topicdataSentPrevious = 0;
 
     namida.warn('Profiler', 'ENABLED');
   }
@@ -20,20 +25,25 @@ class Profiler {
   produceStatistics() {
     let durationSeconds = this.msIntervalProduceStats / 1000;
     if (this.connectionsManager) {
+      let topicdataReceivedCurrent = this.connectionsManager.statistics.counterTopicDataReceived;
+      let topicdataSentCurrent = this.connectionsManager.statistics.counterTopicDataSent;
+
+      let diffTopicDataReceived = topicdataReceivedCurrent - this.topicdataReceivedPrevious;
+      let diffTopicDataSent = topicdataSentCurrent - this.topicdataSentPrevious;
+
       let recordMsgsPerSecondReceived = {
         topic: this.topicStatsMsgsPerSecondSent,
-        double: this.connectionsManager.statistics.counterTopicDataReceived / durationSeconds
+        double: diffTopicDataReceived / durationSeconds
       };
       this.node.publishRecord(recordMsgsPerSecondReceived);
-
       let recordMsgsPerSecondSent = {
         topic: this.topicStatsMsgsPerSecondReceived,
-        double: this.connectionsManager.statistics.counterTopicDataSent / durationSeconds
+        double: diffTopicDataSent / durationSeconds
       };
       this.node.publishRecord(recordMsgsPerSecondSent);
 
-      this.connectionsManager.statistics.counterTopicDataReceived = 0;
-      this.connectionsManager.statistics.counterTopicDataSent = 0;
+      this.topicdataReceivedPrevious = topicdataReceivedCurrent;
+      this.topicdataSentPrevious = topicdataSentCurrent;
 
       if (this.profilingConfig.consoleOutput) {
         namida.log(
